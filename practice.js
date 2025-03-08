c57a072b81bac16555ec25c52b77e847a812c7dc
Banu2403 <banuisgenderli2011@gmail.com>
2025-03-07 23:58:06 -0800

    Add files via upload

--- practice.js
+++ practice.js
@@ -2,23 +2,59 @@
 // function sum(a, b) {
 //     console.log(a + b);
 //     }
-    
+
 //     sum(12, 13 );
-    
+
 //     2ci teleb:
-    
+
 //     - Sum function-i 3cu bir parametr goturecek. Parametrin adi action. action string olacaq
 //     - Eger action === "cem" -> a ve b-ni toplayib console-da gostersin
 //     - Eger action === 'ferq' -> a ve b-ni cixacaqsiz, console-da gosterin
+
+
+
+
+function sum(a, b, action) {
+    switch (action) {
+        case "plus":
+            console.log(a + b)
+            break;
     
-
-
-
-function sum(a,b,action){
-if (action==="sum")
-    console.log(a + b);
-if (action==="minus")
-    console.log(a - b)
+   
+    case "minus":
+        console.log(a - b)
+        break;
+   } 
 }
-sum(1,2,"minus")
+sum(prompt(), prompt(), prompt())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
